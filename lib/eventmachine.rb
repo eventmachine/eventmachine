46e8d9c4b01aae11997a694377df13a0f0651dfd
francis <francis@2c60eb90-88cd-488e-9351-1edcb1d70dbc>
2008-06-15 22:00:00 +0000

    Supported nested calls to EM#run. Many people contributed ideas to this, notably raggi and tmm1.
    git-svn-id: http://rubyeventmachine.com/svn/trunk@697 2c60eb90-88cd-488e-9351-1edcb1d70dbc

--- lib/eventmachine.rb
+++ lib/eventmachine.rb
@@ -213,18 +213,29 @@ module EventMachine
 	# will start without release_machine being called and will immediately throw
 	# a C++ runtime error.
 	#
-	def EventMachine::run &block
-		@conns = {}
-		@acceptors = {}
-		@timers = {}
-		begin
-			@reactor_running = true
-			initialize_event_machine
-			block and add_timer 0, block
-			run_machine
-		ensure
-			release_machine
-			@reactor_running = false
+	def EventMachine::run blk=nil, tail=nil, &block
+		@tails ||= []
+		tail and @tails.unshift(tail)
+
+		if reactor_running?
+			(b = blk || block) and next_tick(b)
+		else
+			@conns = {}
+			@acceptors = {}
+			@timers = {}
+			begin
+				@reactor_running = true
+				initialize_event_machine
+				(b = blk || block) and add_timer(0, b)
+				run_machine
+			ensure
+				release_machine
+				@reactor_running = false
+			end
+
+			until @tails.empty?
+				@tails.pop.call
+			end
 		end
 	end
 
--- tests/test_next_tick.rb
+++ tests/test_next_tick.rb
@@ -55,4 +55,48 @@ class TestNextTick < Test::Unit::TestCase
 		}
 		assert true
 	end
+
+	# This illustrates the solution to a long-standing problem.
+	# It's now possible to correctly nest calls to EM#run.
+	# See the source code commentary for EM#run for more info.
+	#
+	def test_run_run
+		EM.run {
+			EM.run {
+				EM.next_tick {EM.stop}
+			}
+		}
+	end
+
+	# We now support an additional parameter for EM#run.
+	# You can pass two procs to EM#run now. The first is executed as the normal
+	# run block. The second (if given) is scheduled for execution after the
+	# reactor loop completes.
+	# The reason for supporting this is subtle. There has always been an expectation
+	# that EM#run doesn't return until after the reactor loop ends. But now it's
+	# possible to nest calls to EM#run, which means that a nested call WILL
+	# RETURN. In order to write code that will run correctly either way, it's
+	# recommended to put any code which must execute after the reactor completes
+	# in the second parameter.
+	#
+	def test_run_run_2
+		a = proc {EM.stop}
+		b = proc {assert true}
+		EM.run a, b
+	end
+
+
+	# This illustrates that EM#run returns when it's called nested.
+	# This isn't a feature, rather it's something to be wary of when writing code
+	# that must run correctly even if EM#run is called while a reactor is already
+	# running.
+	def test_run_run_3
+		a = []
+		EM.run {
+			EM.run proc {EM.stop}, proc {a << 2}
+			a << 1
+		}
+		assert_equal( [1,2], a )
+	end
+
 end
