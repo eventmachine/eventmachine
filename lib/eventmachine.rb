38533772d0ee5611d9e560c6915e709f41f3ee57
Aman Gupta <aman@tmm1.net>
2009-03-15 16:27:56 -0700

    Updated EchoServer example from README

--- lib/eventmachine.rb
+++ lib/eventmachine.rb
@@ -114,21 +114,28 @@ require 'shellwords'
 # == Usage example
 #
 # Here's a fully-functional echo server implemented in EventMachine:
-# 
-#  require 'rubygems'
+#
 #  require 'eventmachine'
 #
 #  module EchoServer
+#    def post_init
+#      puts "-- someone connected to the echo server!"
+#    end
+#
 #    def receive_data data
 #      send_data ">>>you sent: #{data}"
 #      close_connection if data =~ /quit/i
 #    end
+#
+#    def unbind
+#      puts "-- someone disconnected from the echo server!"
+#    end
 #  end
 #
 #  EventMachine::run {
-#    EventMachine::start_server "192.168.0.100", 8081, EchoServer
+#    EventMachine::start_server "127.0.0.1", 8081, EchoServer
 #  }
-# 
+#
 # What's going on here? Well, we have defined the module EchoServer to
 # implement the semantics of the echo protocol (more about that shortly).
 # The last three lines invoke the event-machine itself, which runs forever
