2f32eacfebeacc98bbe8d9d4be4aa18f6d2f693b
dj2 <dj2@everburning.com>
2010-03-26 15:38:51 -0400

    allow passing args to the deferrable timeout method which will then propagate to the errback
    Signed-off-by: Aman Gupta <aman@tmm1.net>

---
+++ tests/test_deferrable.rb
@@ -0,0 +1,35 @@
+$:.unshift "../lib"
+require 'eventmachine'
+require 'test/unit'
+
+class TestDeferrable < Test::Unit::TestCase
+  class Later
+    include EM::Deferrable
+  end
+
+  def test_timeout_without_args
+    $args = "unset"
+
+    EM.run {
+      df = Later.new
+      df.timeout(0.2)
+      df.errback { $args = "none" }
+      EM.add_timer(0.5) { EM.stop }
+    }
+
+    assert_equal("none", $args)
+  end
+
+  def test_timeout_with_args
+    $args = "unset"
+
+    EM.run {
+      df = Later.new
+      df.timeout(0.2, :timeout, :foo)
+      df.errback { |type, name| $args = [type, name] }
+      EM.add_timer(0.5) { EM.stop }
+    }
+
+    assert_equal([:timeout, :foo], $args)
+  end
+end
