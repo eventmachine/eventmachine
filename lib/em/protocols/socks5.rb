61e67734b37b4369bc43b31b16ebc7ab0fa2f62c
Anton Mironov <ant.mironov@gmail.com>
2010-11-26 11:52:09 +0500

    IPv6 address support

--- lib/em/protocols/socks5.rb
+++ lib/em/protocols/socks5.rb
@@ -62,8 +62,13 @@ module EventMachine
             if @host =~ /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/ # IPv4
               packet << [1, $1.to_i, $2.to_i, $3.to_i, $4.to_i].pack("C*")
             elsif @host =~ /^[\da-f\:]+$/i # IPv6
-              close_connection
-              raise "IPv6 address support is not implemented"
+              l, r = if @host =~ /^(.*)::(.*)$/
+                [$1,$2].map {|i| i.split ":"}
+              else
+                [@host.split(":"),[]]
+              end
+              dec_groups = (l + Array.new(8-l.size-r.size, '0') + r).map {|i| i.hex}
+              packet << ([4] + dec_groups).pack("Cn8")
             else # Domain
               packet << [3, @host.length, @host].pack("CCA*")
             end
