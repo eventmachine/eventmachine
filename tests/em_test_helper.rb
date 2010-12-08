56c65d1b4826228e641ca52cc4e86ca4e32d2bcf
phiggins <pete@peterhiggins.org>
2010-12-07 23:13:05 -0800

    Avoid another test of unsupported behavior on Windows.

--- tests/em_test_helper.rb
+++ tests/em_test_helper.rb
@@ -1,18 +1,20 @@
 require 'eventmachine'
 require 'test/unit'
+require 'rbconfig'
 
 Test::Unit::TestCase.class_eval do
-  def setup_timeout(timeout = 0.5)
+  class EMTestTimeout < StandardError ; end
+
+  def setup_timeout(timeout = 2)
     EM.schedule {
       EM.add_timer(timeout) {
-        raise "timeout" 
+        raise "Test timed out."
       }
     }
   end
 
   # http://blog.emptyway.com/2009/11/03/proper-way-to-detect-windows-platform-in-ruby/
   def self.windows?
-    require 'rbconfig'
     Config::CONFIG['host_os'] =~ /mswin|mingw/
   end
 
--- tests/test_file_watch.rb
+++ tests/test_file_watch.rb
@@ -1,47 +1,58 @@
 require 'em_test_helper'
+require 'tempfile'
 
 class TestFileWatch < Test::Unit::TestCase
-  module FileWatcher
-    def file_modified
-      $modified = true
+  if windows?
+    def test_watch_file_raises_unsupported_error
+      assert_raises(EM::Unsupported) do
+        EM.run do
+          file = Tempfile.new("fake_file")
+          EM.watch_file(file.path)
+        end
+      end
     end
-    def file_deleted
-      $deleted = true
+  else
+    module FileWatcher
+      def file_modified
+        $modified = true
+      end
+      def file_deleted
+        $deleted = true
+      end
+      def unbind
+        $unbind = true
+        EM.stop
+      end
     end
-    def unbind
-      $unbind = true
-      EM.stop
+
+    def setup
+      EM.kqueue = true if EM.kqueue?
     end
-  end
 
-  def setup
-    EM.kqueue = true if EM.kqueue?
-  end
+    def teardown
+      EM.kqueue = false if EM.kqueue?
+    end
 
-  def teardown
-    EM.kqueue = false if EM.kqueue?
-  end
+    def test_events
+      EM.run{
+        file = Tempfile.new('em-watch')
+        $tmp_path = file.path
 
-  def test_events
-    EM.run{
-      require 'tempfile'
-      file = Tempfile.new('em-watch')
-      $tmp_path = file.path
+        # watch it
+        watch = EM.watch_file(file.path, FileWatcher)
+        $path = watch.path
 
-      # watch it
-      watch = EM.watch_file(file.path, FileWatcher)
-      $path = watch.path
+        # modify it
+        File.open(file.path, 'w'){ |f| f.puts 'hi' }
 
-      # modify it
-      File.open(file.path, 'w'){ |f| f.puts 'hi' }
+        # delete it
+        EM.add_timer(0.01){ file.close; file.delete }
+      }
 
-      # delete it
-      EM.add_timer(0.01){ file.close; file.delete }
-    }
-
-    assert_equal($path, $tmp_path)
-    assert($modified)
-    assert($deleted)
-    assert($unbind)
+      assert_equal($path, $tmp_path)
+      assert($modified)
+      assert($deleted)
+      assert($unbind)
+    end
   end
 end
