ad2270cc220e4b18d65634079c7fafa32855bff5
Mitch Williams <mitch@socialcast.com>
2011-02-26 11:56:21 -0800

    Added HTTPs support to HTTPClient

--- lib/em/protocols/httpclient.rb
+++ lib/em/protocols/httpclient.rb
@@ -66,16 +66,18 @@ module EventMachine
       # :host => 'ip/dns', :port => fixnum, :verb => 'GET', :request => 'path',
       # :basic_auth => {:username => '', :password => ''}, :content => 'content',
       # :contenttype => 'text/plain', :query_string => '', :host_header => '',
-      # :cookie => ''
+      # :cookie => '', :ssl => false
       def self.request( args = {} )
-        args[:port] ||= 80
-        EventMachine.connect( args[:host], args[:port], self ) {|c|
-          # According to the docs, we will get here AFTER post_init is called.
-          c.instance_eval {@args = args}
-        }
+        args[:port] ||= args[:ssl] ? 443 : 80
+        EventMachine.connect( args[:host], args[:port], self, args )
+      end
+
+      def initialize(args)
+        @args = args
       end
 
       def post_init
+        start_tls if @args[:ssl]
         @start_time = Time.now
         @data = ""
         @read_state = :base
@@ -87,8 +89,14 @@ module EventMachine
       # NB: This naive technique won't work when we have to support multiple
       # requests on a single connection.
       def connection_completed
+        return if @args[:ssl]
         @connected = true
         send_request @args
+      end
+      
+      def ssl_handshake_completed
+        @connected = true
+        send_request @args        
       end
 
       def send_request args
