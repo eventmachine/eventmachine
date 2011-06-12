d3bbe5a2da8e28c950b49cc233f10bdf229e0e69
Michael S. Klishin <michael@novemberain.com>
2011-06-12 14:10:43 +0400

    EventMachine::FileWatch documentation update

--- lib/em/file_watch.rb
+++ lib/em/file_watch.rb
@@ -1,15 +1,22 @@
 module EventMachine
-
-  # This is subclassed from EventMachine::Connection for use with the file monitoring API. Read the
-  # documentation on the instance methods of this class, and for a full explanation see EventMachine.watch_file.
+  # Utility class that is useful for file monitoring. Supported events are
+  #
+  # * File is modified
+  # * File is deleted
+  # * File is moved
+  #
+  # @see EventMachine.watch_file
   class FileWatch < Connection
-    # :stopdoc:
+    # @private
     Cmodified = 'modified'.freeze
+    # @private
     Cdeleted = 'deleted'.freeze
+    # @private
     Cmoved = 'moved'.freeze
-    # :startdoc:
 
-    def receive_data(data) #:nodoc:
+
+    # @private
+    def receive_data(data)
       case data
       when Cmodified
         file_modified
@@ -20,35 +27,45 @@ module EventMachine
       end
     end
 
-    # Returns the path that EventMachine::watch_file was originally called with. The current implementation
-    # does not pick up on the new filename after a rename occurs.
+    # Returns the path that is being monitored.
+    #
+    # @note Current implementation does not pick up on the new filename after a rename occurs.
+    #
+    # @return [String]
+    # @see EventMachine.watch_file
     def path
       @path
     end
 
-    # Should be redefined with the user's custom callback that will be fired when the file is modified.
+    # Will be called when the file is modified. Supposed to be redefined by subclasses.
+    #
+    # @abstract
     def file_modified
     end
 
-    # Should be redefined with the user's custom callback that will be fired when the file is deleted.
+    # Will be called when the file is deleted. Supposed to be redefined by subclasses.
     # When the file is deleted, stop_watching will be called after this to make sure everything is
     # cleaned up correctly.
     #
-    # Note that on linux (with inotify), file_deleted will not be called until all open file descriptors to
-    # the file have been closed.
+    # @note On Linux (with {http://en.wikipedia.org/wiki/Inotify inotify}), this method will not be called until *all* open file descriptors to
+    #       the file have been closed.
+    #
+    # @abstract
     def file_deleted
     end
 
-    # Should be redefined with the user's custom callback that will be fired when the file is moved or renamed.
+    # Will be called when the file is moved or renamed. Supposed to be redefined by subclasses.
+    #
+    # @abstract
     def file_moved
     end
 
     # Discontinue monitoring of the file.
-    # This involves cleaning up the underlying monitoring details with kqueue/inotify, and in turn firing unbind.
+    #
+    # This involves cleaning up the underlying monitoring details with kqueue/inotify, and in turn firing {EventMachine::Connection#unbind}.
     # This will be called automatically when a file is deleted. User code may call it as well.
     def stop_watching
       EventMachine::unwatch_filename(@signature)
-    end
-  end
-
-end
+    end # stop_watching
+  end # FileWatch
+end # EventMachine
