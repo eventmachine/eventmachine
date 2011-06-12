5fdce25398bd115a63d478498203a208e5410ffc
Michael S. Klishin <michael@novemberain.com>
2011-06-12 13:08:26 +0400

    Improve EventMachine::Channel documentation

--- lib/em/channel.rb
+++ lib/em/channel.rb
@@ -1,33 +1,38 @@
 module EventMachine
-  # Provides a simple interface to push items to a number of subscribers. The
-  # channel will schedule all operations on the main reactor thread for thread
-  # safe reactor operations.
+  # Provides a simple thread-safe way to transfer data between (typically) long running
+  # tasks in {EventMachine.defer} and event loop thread.
   #
-  # This provides a convenient way for connections to consume messages from 
-  # long running code in defer, without threading issues.
+  # @example
   #
-  #  channel = EM::Channel.new
-  #  sid = channel.subscribe{ |msg| p [:got, msg] }
+  #  channel = EventMachine::Channel.new
+  #  sid     = channel.subscribe { |msg| p [:got, msg] }
+  #
   #  channel.push('hello world')
   #  channel.unsubscribe(sid)
   #
-  # See examples/ex_channel.rb for a detailed example.
+  #
   class Channel
-    # Create a new channel
     def initialize
       @subs = {}
-      @uid = 0
+      @uid  = 0
     end
 
     # Takes any arguments suitable for EM::Callback() and returns a subscriber
     # id for use when unsubscribing.
+    #
+    # @return [Integer] Subscribe identifier
+    # @see #unsubscribe
     def subscribe(*a, &b)
       name = gen_id
       EM.schedule { @subs[name] = EM::Callback(*a, &b) }
+
       name
     end
 
-    # Removes this subscriber from the list.
+    # Removes subscriber from the list.
+    #
+    # @param [Integer] Subscriber identifier
+    # @see #subscribe
     def unsubscribe(name)
       EM.schedule { @subs.delete name }
     end
@@ -39,7 +44,7 @@ module EventMachine
     end
     alias << push
 
-    # Receive exactly one message from the channel.
+    # Fetches one message from the channel.
     def pop(*a, &b)
       EM.schedule {
         name = subscribe do |*args|
@@ -50,7 +55,9 @@ module EventMachine
     end
 
     private
-    def gen_id # :nodoc:
+
+    # @private
+    def gen_id
       @uid += 1
     end
   end
