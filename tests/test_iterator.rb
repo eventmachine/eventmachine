2caaf73a7197ab56319201bc8ebb3b6e91a26262
Petr <pedro.yanoviches@gmail.com>
2012-02-20 15:54:25 +0400

    Added support for Enumerable in 1.9 and backport in 1.8; Unit Tests added

--- tests/test_iterator.rb
+++ tests/test_iterator.rb
@@ -1,5 +1,4 @@
 require 'em_test_helper'
-require 'minitest/spec'
 
 class TestEnumerable
   include Enumerable
@@ -11,13 +10,101 @@ class TestEnumerable
   end
 end
 
-describe EventMachine::Iterator do
-  it "should go on" do
-    EM::Iterator.new(0..10).each{ |num,iter| iter.next }
+class TestEnumerable2
+  include Enumerable
+
+  def each
+    arr = ('a'..'g').to_a
+    while it = arr.shift do
+      yield it
+    end
+  end
+end
+
+class TestEnumerable3
+  include Enumerable
+
+  def each
+    ('a'..'f').each.with_index do |ltr, index|
+      break if ltr == 'e'
+      eval("@#{ltr} = #{index}")
+      yield ltr
+    end
+  end
+end
+
+class TestIterator < Test::Unit::TestCase
+
+  def test_iterator_with_array
+    assert_nothing_raised do
+      EM.run {
+        after = proc{ EM.stop }
+        EM::Iterator.new(0..10, 10).each(nil, after){ |num,iter| iter.next }
+      }
+    end
   end
 
-  it "should works with lazy enumerable" do
-    en = TestEnumerable.new
-    EM::Iterator.new(en).each{ |num, iter| iter.next }
+  def test_iterator_with_array_with_result
+    nums = []
+    EM.run {
+      after = proc{ EM.stop }
+      EM::Iterator.new((0..10)).each(nil, after){ |num,iter|
+        nums << num
+        iter.next
+      }
+    }
+    res = (0..10).to_a
+    assert_equal res, nums
+  end
+
+  def test_iterator_with_enumerable
+    assert_nothing_raised do
+      EM.run {
+        en = TestEnumerable.new
+        after = proc{ EM.stop }
+        EM::Iterator.new(en, 10).each(nil, after){ |num, iter| iter.next }
+      }
+    end
+  end
+
+  def test_iterator_with_enumerable_with_result
+    letters = []
+    EM.run {
+      en = TestEnumerable2.new
+      after = proc{ EM.stop }
+      EM::Iterator.new(en, 10).each(nil, after){ |ltr,iter|
+        letters << ltr
+        iter.next
+      }
+    }
+    res = ('a'..'g').to_a
+    assert_equal res, letters
+  end
+
+  def test_iterator_with_array_with_nils
+    nums = []
+    EM.run {
+      after = proc{ EM.stop }
+      EM::Iterator.new(["Hello", nil, "World", nil]).each(nil, after){ |num,iter|
+        nums << num
+        iter.next
+      }
+    }
+    res = "Hello World"
+    assert_equal res, nums.compact.join(" ")
+  end
+
+  def test_iterator_for_lazyness
+    enumerable = TestEnumerable3.new
+    enumerator = enumerable.to_enum
+    EM.run {
+      after = proc{ EM.stop }
+      EM::Iterator.new(enumerator).each(nil, after){ |num,iter|
+        iter.next
+      }
+    }
+    assert_equal 0, enumerable.instance_variable_get(:@a)
+    assert_equal 3, enumerable.instance_variable_get(:@d)
+    assert_equal false, enumerable.instance_variable_defined?(:@e)
   end
 end
