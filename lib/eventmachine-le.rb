f0c6a2d9da5d24683b69b16ec4bb655487fa13b8
Iñaki Baz Castillo <ibc@aliax.net>
2012-02-24 12:48:50 +0100

    Library loading improved (no more absolute paths, instead "gem 'eventmachine-le'; require 'eventmachine'" is added to lib/eventmachine-le.rb file.

--- lib/eventmachine-le.rb
+++ lib/eventmachine-le.rb
@@ -1,6 +1,10 @@
-# This file allows usage of:
+# This file allows usage of
 #
 #   require "eventmachine-le"
 #
+# and ensures that the eventmachine.rb and em/xxx.rb loaded files are those
+# within eventmachine-le Gem rather than the official eventmachine Gem files
+# (if installed).
 
-require ::File.join(::Dir.pwd, "eventmachine.rb")
+gem 'eventmachine-le'
+require 'eventmachine'
