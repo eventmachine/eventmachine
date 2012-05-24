a01dbc24f19dd24f8515279bf61f6129adcc996f
Burke Libbey <burke@burkelibbey.org>
2012-05-24 15:58:44 -0500

    Improved SSL verification tests and added new certificates

--- tests/test_ssl_verify.rb
+++ tests/test_ssl_verify.rb
@@ -3,17 +3,35 @@ require 'em_test_helper'
 if EM.ssl?
   class TestSslVerify < Test::Unit::TestCase
     CERT_DIRECTORY = File.expand_path("../certs", __FILE__) + '/'
-    CLIENT_KEY_FILE  = CERT_DIRECTORY + 'client.key'
-    CLIENT_CERT_FILE = CERT_DIRECTORY + 'client.crt'
-    AUTHORITATIVE_CACERT = File.expand_path('../../cacert.pem', __FILE__)
 
-    def cert_from_file
-      File.read(CLIENT_CERT_FILE)
+    module Server
+      def post_init
+        start_tls(
+          :private_key_file => CERT_DIRECTORY + "#$cert.key",
+          :cert_chain_file  => CERT_DIRECTORY + "#$cert.crt",
+          :private_key_pwd  => 'asdf'
+        )
+      end
+
+      def ssl_handshake_completed
+        $server_handshake_completed = true
+      end
     end
 
     module Client
-      def connection_completed
-        start_tls(:private_key_file => CLIENT_KEY_FILE, :cert_chain_file => CLIENT_CERT_FILE)
+      def post_init
+        start_tls(:verify_peer => true, :ca_file => CERT_DIRECTORY + 'cacert.pem', :hostname => $name)
+      end
+
+      def ssl_verify_peer(cert, preverify_ok)
+        $cert_from_server = cert
+        $preverify_result = preverify_ok
+        if $stub_verify != nil
+          ret = $stub_verify
+          $stub_verify = nil
+          return ret
+        end
+        preverify_ok
       end
 
       def ssl_handshake_completed
@@ -26,86 +44,62 @@ if EM.ssl?
       end
     end
 
-    module AcceptServer
-      def post_init
-        start_tls(:verify_peer => true)
-      end
-
-      def ssl_verify_peer(cert)
-        $cert_from_server = cert
-        true
-      end
-
-      def ssl_handshake_completed
-        $server_handshake_completed = true
-      end
-    end
-
-    module DenyServer
-      def post_init
-        start_tls(:verify_peer => true)
-      end
-
-      def ssl_verify_peer(cert)
-        $cert_from_server = cert
-        # Do not accept the peer. This should now cause the connection to shut down without the SSL handshake being completed.
-        false
-      end
-
-      def ssl_handshake_completed
-        $server_handshake_completed = true
-      end
-    end
-
-    module WebClient
-      def post_init
-        start_tls(:verify_peer => true, :ca_file => AUTHORITATIVE_CACERT, :hostname => $name)
-      end
-
-      def ssl_verify_peer(cert, preverify_ok)
-        $preverify_result = preverify_ok
-        EM.stop_event_loop
-      end
-    end
-
-    def test_against_legitimate
-      $name = "www.facebook.com"
-      EM.run { EM.connect($name, 443, WebClient) }
+    def test_valid_certificates_pass_preverification
+      $name = "127.0.0.1"
+      $cert = "ca-signed-127.0.0.1"
+      EM.run {
+        EM.start_server($name, 16784, Server)
+        EM.connect($name, 16784, Client)
+      }
       assert $preverify_result
     end
 
-    def test_against_wrong_cn
-      $name = "promanagerstaging.parentyreitmeier.com"
-      EM.run { EM.connect($name, 443, WebClient) }
-      refute $preverify_result
-    end
-
-    def test_against_self_signed
-      $name = "cryptoanarchy.org"
-      EM.run { EM.connect($name, 443, WebClient) }
-      refute $preverify_result
-    end
-
-    def test_accept_server
-      $client_handshake_completed, $server_handshake_completed = false, false
+    def test_certificates_with_incorrect_common_names_fail_preverification
+      $name = "127.0.0.1"
+      $cert = "ca-signed-eventmachine"
       EM.run {
-        EM.start_server("127.0.0.1", 16784, AcceptServer)
+        EM.start_server($name, 16784, Server)
+        EM.connect($name, 16784, Client)
+      }
+      refute $preverify_result
+    end
+
+    def test_self_signed_certificates_fail_preverification
+      $name = "EventMachine"
+      $cert = "self-signed-eventmachine"
+      EM.run {
+        EM.start_server("127.0.0.1", 16784, Server)
+        EM.connect("127.0.0.1", 16784, Client)
+      }
+      refute $preverify_result
+    end
+
+    def test_accepting_connection
+      $client_handshake_completed, $server_handshake_completed = false, false
+      $stub_verify = true
+      $name = "127.0.0.1"
+      $cert = "ca-signed-127.0.0.1"
+      EM.run {
+        EM.start_server("127.0.0.1", 16784, Server)
         EM.connect("127.0.0.1", 16784, Client)
       }
 
-      assert_equal(cert_from_file, $cert_from_server)
+      cert = File.read(CERT_DIRECTORY + 'ca-signed-127.0.0.1.crt')
+      assert_equal(cert, $cert_from_server)
       assert($client_handshake_completed)
       assert($server_handshake_completed)
     end
 
-    def test_deny_server
+    def test_rejecting_connection
       $client_handshake_completed, $server_handshake_completed = false, false
+      $stub_verify = false
+      $name = "127.0.0.1"
+      $cert = "ca-signed-127.0.0.1"
       EM.run {
-        EM.start_server("127.0.0.1", 16784, DenyServer)
+        EM.start_server("127.0.0.1", 16784, Server)
         EM.connect("127.0.0.1", 16784, Client)
       }
 
-      assert_equal(cert_from_file, $cert_from_server)
       assert(!$client_handshake_completed)
       assert(!$server_handshake_completed)
     end
