af6b97d8e0c89135283a651aa3ca9814eab9c586
Noah Luck Easterly <noah.easterly@gmail.com>
2013-01-26 16:44:37 -0500

    refactoring ssl-verify tests to get rid of globals and make it easier to test more options

--- tests/test_ssl_verify.rb
+++ tests/test_ssl_verify.rb
@@ -2,79 +2,99 @@ require 'em_test_helper'
 
 if EM.ssl?
   class TestSslVerify < Test::Unit::TestCase
+    attr_writer :client_handshake_completed
+    attr_writer :server_handshake_completed
+    attr_writer :cert_from_server
+
     def setup
-      $dir = File.dirname(File.expand_path(__FILE__)) + '/'
-      $cert_from_file = File.read($dir+'client.crt')
+      @dir = File.dirname(File.expand_path(__FILE__)) + '/'
+      @cert_from_file = File.read(@dir+'client.crt')
+      @client_handshake_completed = false
+      @server_handshake_completed = false
     end
 
-    module Client
-      def connection_completed
-        start_tls(:private_key_file => $dir+'client.key', :cert_chain_file => $dir+'client.crt')
-      end
-
-      def ssl_handshake_completed
-        $client_handshake_completed = true
-        close_connection
-      end
-
-      def unbind
-        EM.stop_event_loop
-      end
-    end
-
-    module AcceptServer
-      def post_init
-        start_tls(:verify_peer => true)
-      end
-
-      def ssl_verify_peer(cert)
-        $cert_from_server = cert
-        true
-      end
-
-      def ssl_handshake_completed
-        $server_handshake_completed = true
+    # create a TLS client module that records (in this test)
+    # whether the handshake was completed
+    def client
+      test = self
+      tls_options = { 
+        :private_key_file => @dir+'client.key', 
+        :cert_chain_file => @dir+'client.crt'
+      }
+      @client_handshake_completed = false
+      Module.new do
+        define_method :connection_completed do
+          start_tls(tls_options)
+        end
+        define_method :ssl_handshake_completed do
+          test.client_handshake_completed = true
+          close_connection
+        end
+        define_method :unbind do
+          EM.stop_event_loop
+        end
       end
     end
 
-    module DenyServer
-      def post_init
-        start_tls(:verify_peer => true)
-      end
-
-      def ssl_verify_peer(cert)
-        $cert_from_server = cert
-        # Do not accept the peer. This should now cause the connection to shut down without the SSL handshake being completed.
-        false
-      end
-
-      def ssl_handshake_completed
-        $server_handshake_completed = true
+    # create a TLS server module that records (in this test) 
+    # whether the handshake was completed, 
+    # and defers to the caller for ssl_verify_peer
+    def server(tls_options, &ssl_verify_peer)
+      test = self
+      Module.new do
+        define_method :post_init do
+          start_tls(tls_options)
+        end
+        if ssl_verify_peer
+          define_method :ssl_verify_peer, &ssl_verify_peer
+        end
+        define_method :ssl_handshake_completed do
+          test.server_handshake_completed = true
+        end
       end
     end
+    
+    # the cert that ssl_verify_peer gets should be the same one
+    # that the client uses
+    def test_cert_passthrough
+      test = self
+      EM.run do
+        EM.start_server("127.0.0.1", 16784, server(:verify_peer => true) { |cert|
+          test.cert_from_server = cert
+          true
+        })
+        EM.connect("127.0.0.1", 16784, client).instance_variable_get("@signature")
+      end
 
+      assert_equal(@cert_from_file, @cert_from_server)
+    end
+
+    # accepting all certs should complete the handshake
     def test_accept_server
-      $client_handshake_completed, $server_handshake_completed = false, false
-      EM.run {
-        EM.start_server("127.0.0.1", 16784, AcceptServer)
-        EM.connect("127.0.0.1", 16784, Client).instance_variable_get("@signature")
-      }
+      EM.run do
+        EM.start_server("127.0.0.1", 16784, server(:verify_peer => true) { |cert|
+          true
+        })
+        EM.connect("127.0.0.1", 16784, client).instance_variable_get("@signature")
+      end
 
-      assert_equal($cert_from_file, $cert_from_server)
-      assert($client_handshake_completed)
-      assert($server_handshake_completed)
+      assert(@client_handshake_completed)
+      assert(@server_handshake_completed)
     end
 
+    # denying all certs should never complete the handshake
     def test_deny_server
-      $client_handshake_completed, $server_handshake_completed = false, false
-      EM.run {
-        EM.start_server("127.0.0.1", 16784, DenyServer)
-        EM.connect("127.0.0.1", 16784, Client)
-      }
+      EM.run do
+        EM.start_server("127.0.0.1", 16784, server(:verify_peer => true) { |cert|
+          # Do not accept the peer. This should now cause the connection to
+          # shut down without the SSL handshake being completed.
+          false
+        })
+        EM.connect("127.0.0.1", 16784, client)
+      end
 
-      assert_equal($cert_from_file, $cert_from_server)
-      assert(!$client_handshake_completed)
-      assert(!$server_handshake_completed)
+      assert(!@client_handshake_completed)
+      assert(!@server_handshake_completed)
     end
   end
 else
