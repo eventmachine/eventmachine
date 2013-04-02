61095b48790ff63ea6aebf751c282332016f7a53
Aman Gupta <aman@tmm1.net>
2013-04-02 13:37:49 -0700

    fix idle test with epoll (cc #427)

--- tests/test_idle_connection.rb
+++ tests/test_idle_connection.rb
@@ -9,15 +9,17 @@ class TestIdleConnection < Test::Unit::TestCase
           $idle_time = conn.get_idle_time
           conn.send_data "GET / HTTP/1.0\r\n\r\n"
           EM.next_tick{
-            $idle_time_after_send = conn.get_idle_time
-            conn.close_connection
-            EM.stop
+            EM.next_tick{
+              $idle_time_after_send = conn.get_idle_time
+              conn.close_connection
+              EM.stop
+            }
           }
         }
       }
 
       assert_in_delta 3, $idle_time, 0.2
-      assert_equal 0, $idle_time_after_send
+      assert_in_delta 0, $idle_time_after_send, 0.1
     end
   end
 end
