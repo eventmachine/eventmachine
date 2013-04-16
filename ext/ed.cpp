84b3d4d05d16fa2b9200705c096a18c5c0e1039b
spatulasnout <billk@cts.com>
2013-04-15 18:23:18 -0700

    Bug fix for OS_WIN32: Replaced errno with WSAGetLastError() where result is compared with WSAE... codes.
    Per http://msdn.microsoft.com/en-us/library/windows/desktop/ms737828%28v=vs.85%29.aspx
    Error codes set by Windows Sockets are not made available through the
    errno variable.
    
    Comparison of errno values with WSAE codes was causing EventMachine
    to spontaneously close socket connections when the errno value didn't
    match the expected WSAE codes.

--- ext/ed.cpp
+++ ext/ed.cpp
@@ -775,7 +775,11 @@ void ConnectionDescriptor::Read()
 		
 
 		int r = read (sd, readbuffer, sizeof(readbuffer) - 1);
+#ifdef OS_WIN32
+		int e = WSAGetLastError();
+#else
 		int e = errno;
+#endif
 		//cerr << "<R:" << r << ">";
 
 		if (r > 0) {
@@ -1042,7 +1046,11 @@ void ConnectionDescriptor::_WriteOutboundData()
 	#endif
 
 	bool err = false;
+#ifdef OS_WIN32
+	int e = WSAGetLastError();
+#else
 	int e = errno;
+#endif
 	if (bytes_written < 0) {
 		err = true;
 		bytes_written = 0;
@@ -1673,7 +1681,11 @@ void DatagramDescriptor::Write()
 
 		// The nasty cast to (char*) is needed because Windows is brain-dead.
 		int s = sendto (sd, (char*)op->Buffer, op->Length, 0, (struct sockaddr*)&(op->From), sizeof(op->From));
+#ifdef OS_WIN32
+		int e = WSAGetLastError();
+#else
 		int e = errno;
+#endif
 
 		OutboundDataSize -= op->Length;
 		op->Free();
