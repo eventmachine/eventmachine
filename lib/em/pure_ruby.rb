82df85f25c4b7539c04858118ecdaea8bd337804
noice <noice@email.cz>
2013-05-10 13:26:46 +0200

    Use 127.0.0.1 instead of localhost for em/pure_ruby bind

--- lib/em/pure_ruby.rb
+++ lib/em/pure_ruby.rb
@@ -393,7 +393,7 @@ module EventMachine
       100.times {
         @loopbreak_port = rand(10000) + 40000
         begin
-          @loopbreak_reader.bind "localhost", @loopbreak_port
+          @loopbreak_reader.bind "127.0.0.1", @loopbreak_port
           bound = true
           break
         rescue
