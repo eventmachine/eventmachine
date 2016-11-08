7781236314a48fb8566832802c0aaabff29f6ad4
Donovan Lampa <donovan.lampa@gmail.com>
2016-11-08 12:12:00 -0600

    Use Acceptors to get peer and sock names if not present in Connections (#743)
    * Get Peername and sockname from acceptors if connection not available
    * Add missing Constant for jeventmachine

--- java/src/com/rubyeventmachine/EmReactor.java
+++ java/src/com/rubyeventmachine/EmReactor.java
@@ -521,11 +521,27 @@ public class EmReactor {
 	}
 
 	public Object[] getPeerName (long sig) {
-		return Connections.get(sig).getPeerName();
+	    EventableChannel channel = Connections.get(sig);
+	    if (channel != null) {
+	        return Connections.get(sig).getPeerName();
+	    }
+	    else { 
+	        ServerSocketChannel acceptor = Acceptors.get(sig);
+	        return  new Object[] { acceptor.socket().getLocalPort(),
+	                acceptor.socket().getInetAddress().getHostAddress() };   
+	    }
 	}
 
 	public Object[] getSockName (long sig) {
-		return Connections.get(sig).getSockName();
+    	EventableChannel channel = Connections.get(sig);
+    	if (channel != null) {
+    	    return Connections.get(sig).getSockName();
+    	}
+    	else {
+    	    ServerSocketChannel acceptor = Acceptors.get(sig);
+    	    return new Object[] { acceptor.socket().getLocalPort(),
+    	            acceptor.socket().getInetAddress().getHostAddress() };
+    	}
 	}
 
 	public long attachChannel (SocketChannel sc, boolean watch_mode) {
