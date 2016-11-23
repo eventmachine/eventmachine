79b091e583acec61c2d95d84fff5c31993faad66
Aaron Stone <aaron@serendipity.cx>
2016-11-23 00:13:57 -0800

    Make the iterator tests faster and more robust

--- tests/test_iterator.rb
+++ tests/test_iterator.rb
@@ -2,8 +2,12 @@ require 'em_test_helper'
 
 class TestIterator < Test::Unit::TestCase
 
-  def get_time
-    EM.current_time.strftime('%H:%M:%S')
+  # By default, format the time with tenths-of-seconds.
+  # Some tests should ask for extra decimal places to ensure
+  # that delays between iterations will receive a changed time.
+  def get_time(n=1)
+    time = EM.current_time
+    time.strftime('%H:%M:%S.') + time.tv_usec.to_s[0, n]
   end
 
   def test_default_concurrency
@@ -11,10 +15,10 @@ class TestIterator < Test::Unit::TestCase
     list = 1..10
     EM.run {
       EM::Iterator.new(list).each( proc {|num,iter|
-        time = get_time
+        time = get_time(3)
         items[time] ||= []
         items[time] << num
-        EM::Timer.new(1) {iter.next}
+        EM::Timer.new(0.02) {iter.next}
       }, proc {EM.stop})
     }
     assert_equal(10, items.keys.size)
@@ -27,10 +31,10 @@ class TestIterator < Test::Unit::TestCase
     original_list = list.dup
     EM.run {
       EM::Iterator.new(proc{list.pop || EM::Iterator::Stop}).each( proc {|num,iter|
-        time = get_time
+        time = get_time(3)
         items[time] ||= []
         items[time] << num
-        EM::Timer.new(1) {iter.next}
+        EM::Timer.new(0.02) {iter.next}
       }, proc {EM.stop})
     }
     assert_equal(10, items.keys.size)
@@ -52,26 +56,25 @@ class TestIterator < Test::Unit::TestCase
     assert_equal(list.to_a.sort, items.values.flatten.sort)
   end
 
-
   def test_changing_concurrency_affects_active_iteration
     items = {}
     list = 1..25
+    seen = 0
     EM.run {
-      i = EM::Iterator.new(list,5)
+      i = EM::Iterator.new(list,1)
       i.each(proc {|num,iter|
         time = get_time
         items[time] ||= []
         items[time] << num
-        EM::Timer.new(1) {iter.next}
+        if (seen += 1) == 5
+          # The first 5 items will be distinct times
+          # The next 20 items will happen in 2 bursts
+          i.concurrency = 10
+        end
+        EM::Timer.new(0.2) {iter.next}
       }, proc {EM.stop})
-      EM.add_timer(1){
-        i.concurrency = 1
-      }
-      EM.add_timer(3){
-        i.concurrency = 3
-      }
     }
-    assert_equal(9, items.keys.size)
+    assert_in_delta(7, items.keys.size, 1)
     assert_equal(list.to_a.sort, items.values.flatten.sort)
   end
 
