ca5d6e5c9147598c3edd8c1cf700bcdeb74398a3
Aaron Stone <aaron@serendipity.cx>
2016-11-22 19:01:36 -0800

    Use a well-known black-hole address range instead of Google

--- tests/test_unbind_reason.rb
+++ tests/test_unbind_reason.rb
@@ -1,5 +1,4 @@
 require 'em_test_helper'
-require 'socket'
 
 class TestUnbindReason < Test::Unit::TestCase
 
@@ -11,42 +10,31 @@ class TestUnbindReason < Test::Unit::TestCase
     end
   end
 
+  # RFC 5737 Address Blocks Reserved for Documentation
   def test_connect_timeout
-    error = nil
-    EM.run {
-      conn = EM.connect 'google.com', 81, Module.new{ |m|
-        m.send(:define_method, :unbind) do |reason|
-          error = reason
-          EM.stop
-        end
-      }
-      conn.pending_connect_timeout = TIMEOUT_INTERVAL
-    }
-    assert_equal Errno::ETIMEDOUT, error
+    conn = nil
+    EM.run do
+      conn = EM.connect '192.0.2.0', 80, StubConnection
+      conn.pending_connect_timeout = 1
+    end
+    assert_equal Errno::ETIMEDOUT, conn.error
   end
 
   def test_connect_refused
     pend('FIXME: this test is broken on Windows') if windows?
-
-    error = nil
-    EM.run {
-      EM.connect '127.0.0.1', 12388, Module.new{ |m|
-        m.send(:define_method, :unbind) do |reason|
-          error = reason
-          EM.stop
-        end
-      }
-    }
-    assert_equal Errno::ECONNREFUSED, error
+    conn = nil
+    EM.run do
+      conn = EM.connect '127.0.0.1', 12388, StubConnection
+    end
+    assert_equal Errno::ECONNREFUSED, conn.error
   end
 
   def test_optional_argument
     pend('FIXME: this test is broken on Windows') if windows?
-
     conn = nil
-    EM.run {
+    EM.run do
       conn = EM.connect '127.0.0.1', 12388, StubConnection
-    }
+    end
     assert_equal Errno::ECONNREFUSED, conn.error
   end
 end
