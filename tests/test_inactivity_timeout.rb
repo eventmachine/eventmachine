cff3b48974a486237d6631c05247a0e2d8a0c442
jolan <jolan@users.noreply.github.com>
2019-05-24 14:50:17 -0500

    add failing test where comm_inactivity_timeout takes too long (#889)

--- tests/test_inactivity_timeout.rb
+++ tests/test_inactivity_timeout.rb
@@ -21,7 +21,7 @@ class TestInactivityTimeout < Test::Unit::TestCase
     end
 
     def test_for_real
-      start, finish = nil
+      start, finish, reason = nil
 
       timeout_start = Module.new do
         define_method :post_init do
@@ -47,6 +47,49 @@ class TestInactivityTimeout < Test::Unit::TestCase
       }
       # Travis can vary from 0.02 to 0.17, Appveyor maybe as low as 0.01
       assert_in_delta(0.09, (finish - start), 0.08)
+
+      # simplified reproducer for comm_inactivity_timeout taking twice as long
+      # as requested -- https://github.com/eventmachine/eventmachine/issues/554
+      timeout_start_tls = Module.new do
+        define_method :post_init do
+          start = Time.now
+          start_tls
+        end
+        define_method :receive_data do |data|
+          send_data ">>>you sent: #{data}"
+        end
+      end
+
+      timeout_handler_tls = Module.new do
+        define_method :connection_completed do
+          start_tls
+        end
+
+        define_method :ssl_handshake_completed do
+          @timer = EM::PeriodicTimer.new(0.05) do
+            #puts "get_idle_time: #{get_idle_time} inactivity: #{comm_inactivity_timeout}"
+          end
+          send_data "hello world"
+        end
+
+        define_method :unbind do |r|
+          finish = Time.now
+          reason = r
+          EM.stop
+        end
+      end
+
+      EM.run {
+        setup_timeout 1.4
+        EM.start_server("127.0.0.1", 12345, timeout_start_tls)
+        c = EM.connect("127.0.0.1", 12345, timeout_handler_tls)
+        c.comm_inactivity_timeout = 0.15
+      }
+
+      # .30 is double the timeout and not acceptable
+      assert_in_delta(0.15, (finish - start), 0.14)
+      # make sure it was a timeout and not a TLS error
+      assert_equal Errno::ETIMEDOUT, reason
     end
   else
     warn "EM.comm_inactivity_timeout not implemented, skipping tests in #{__FILE__}"
