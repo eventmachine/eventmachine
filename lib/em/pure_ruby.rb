e29ccdefbf4b753dfec513c2df17b3bf1b827425
wordjelly <bhargav.r.raut@gmail.com>
2020-09-06 10:02:31 +0530

    fix: em/pure_ruby intra-iteration element delete bug (#929)

--- lib/em/pure_ruby.rb
+++ lib/em/pure_ruby.rb
@@ -569,14 +569,18 @@ module EventMachine
     end
 
     def run_timers
+      timers_to_delete = []
       @timers.each {|t|
         if t.first <= @current_loop_time
-          @timers.delete t
+          #@timers.delete t
+          timers_to_delete << t
           EventMachine::event_callback "", TimerFired, t.last
         else
           break
         end
       }
+      timers_to_delete.map{|c| @timers.delete c}
+      timers_to_delete = nil
       #while @timers.length > 0 and @timers.first.first <= now
       #  t = @timers.shift
       #  EventMachine::event_callback "", TimerFired, t.last
