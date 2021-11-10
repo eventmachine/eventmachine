25cd75b946c761fb8070f5baaf8953aee87bbea0
nicholas a. evans <nicholas.evans@gmail.com>
2021-11-10 18:22:04 -0500

    squashed ext/ updates

--- ext/ssl.h
+++ ext/ssl.h
@@ -21,8 +21,9 @@ See the file COPYING for complete licensing information.
 #ifndef __SslBox__H_
 #define __SslBox__H_
 
-
-
+#include "page.h"
+#include <openssl/ossl_typ.h>
+#include <string>
 
 #ifdef WITH_SSL
 
@@ -30,14 +31,57 @@ See the file COPYING for complete licensing information.
 class SslContext_t
 ******************/
 
+// "SSLContext < Struct" => em_ssl_ctx_t => SslContext_t => SSL_CTX
+// This can definitely be simplified!
+
+/* This struct, em_ssl_ctx, is used simply to pass values from the ruby API
+ * layer into this C++ layer while keeping them seperated.  Is this useful?
+ * Probably it's not.
+ *
+ * Proposal: the immediate SSL_CTX wrapping functionality should be moved into
+ * a near-duplicate of stdlib's OpenSSL::SSL::SSLContext.  i.e. EM::SSL::Context
+ * should use TypedData_Get_Struct around an SSL_CTX object.
+ *
+ * With that done, this C++ layer should probably just be deleted.  It won't be
+ * doing much.  The other EM C++ code can reference the SSL_CTX object using the
+ * openssl API, or (where necessary) use the ruby VALUE wrapper via
+ * SSL_CTX_get_ex_data.
+ *
+ */
+typedef struct em_ssl_ctx {
+	int min_proto_version;
+	int max_proto_version;
+	unsigned long options;
+	int verify_mode;
+	bool verify_hostname;
+
+	/* X509_STORE *cert_store; */
+	bool cert_store;
+	const char *ca_file;
+	const char *ca_path;
+
+	const char *cert;
+	const char *cert_chain_file;
+	const char *key;
+	const char *private_key_file;
+	const char *private_key_pass;
+	int  private_key_pass_len;
+
+	const char *ciphers;
+	const char *ecdh_curve;
+	const char *dhparam;
+} em_ssl_ctx_t;
+
 class SslContext_t
 {
 	public:
-		SslContext_t (bool is_server, const std::string &privkeyfile, const std::string &privkey, const std::string &privkeypass, const std::string &certchainfile, const std::string &cert, const std::string &cipherlist, const std::string &ecdh_curve, const std::string &dhparam, int ssl_version);
+		SslContext_t (bool is_server, const em_ssl_ctx_t *context);
 		virtual ~SslContext_t();
+		bool bVerifyHostname;
 
 	private:
 		static bool bLibraryInitialized;
+		static X509_STORE* bDefaultX509Store;
 
 	private:
 		bool bIsServer;
@@ -61,9 +105,14 @@ class SslBox_t
 class SslBox_t
 {
 	public:
-		SslBox_t (bool is_server, const std::string &privkeyfile, const std::string &privkey, const std::string &privkeypass, const std::string &certchainfile, const std::string &cert, bool verify_peer, bool fail_if_no_peer_cert, const std::string &snihostname, const std::string &cipherlist, const std::string &ecdh_curve, const std::string &dhparam, int ssl_version, const uintptr_t binding);
+		SslBox_t (
+				bool is_server,
+				const std::string &snihostname,
+				const SslContext_t *context,
+				const uintptr_t binding);
 		virtual ~SslBox_t();
 
+		const std::string SniHostname;
 		int PutPlaintext (const char*, int);
 		int GetPlaintext (char*, int);
 
@@ -71,6 +120,8 @@ class SslBox_t
 		bool CanGetCiphertext();
 		int GetCiphertext (char*, int);
 		bool IsHandshakeCompleted() {return bHandshakeCompleted;}
+
+		int VerifyPeer(bool, X509_STORE_CTX *);
 
 		X509 *GetPeerCert();
 		int GetCipherBits();
@@ -81,12 +132,10 @@ class SslBox_t
 		void Shutdown();
 
 	protected:
-		SslContext_t *Context;
+		const SslContext_t *Context;
 
 		bool bIsServer;
 		bool bHandshakeCompleted;
-		bool bVerifyPeer;
-		bool bFailIfNoPeerCert;
 		SSL *pSSL;
 		BIO *pbioRead;
 		BIO *pbioWrite;
@@ -94,7 +143,7 @@ class SslBox_t
 		PageList OutboundQ;
 };
 
-extern "C" int ssl_verify_wrapper(int, X509_STORE_CTX*);
+extern "C" int em_ossl_ssl_verify_callback(int, X509_STORE_CTX*);
 
 #endif // WITH_SSL
 
