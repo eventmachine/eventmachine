b1f3704e5de167611c588df70cc30b89a88d2257
MSP-Greg <Greg.mpls@gmail.com>
2022-05-13 18:12:11 -0500

    Use SSL_CTX_set_dh_auto if needed and available

--- tests/test_ssl_dhparam.rb
+++ tests/test_ssl_dhparam.rb
@@ -12,17 +12,22 @@ class TestSSLDhParam < Test::Unit::TestCase
   DH_1_2 =   { cipher_list: "DHE,EDH", ssl_version: %w(TLSv1_2) }
   CLIENT_1_2 = { client_unbind: true,  ssl_version: %w(TLSv1_2) }
 
-  def test_no_dhparam
+  def test_dhparam_1_2_auto
     omit_if(EM.library_type == :pure_ruby) # DH will work with defaults
     omit_if(rbx?)
 
     client_server client: CLIENT_1_2, server: DH_1_2
 
-    refute Client.handshake_completed?
-    refute Server.handshake_completed?
+    if EMSSLHandlers::IS_SSL_GE_1_1
+      assert Client.handshake_completed?
+      assert Server.handshake_completed?
+    else
+      refute Client.handshake_completed?
+      refute Server.handshake_completed?
+    end
   end
 
-  def test_dhparam_1_2
+  def test_dhparam_1_2_supplied
     omit_if(rbx?)
 
     client_server client: CLIENT_1_2, server: DH_1_2.merge(dhparam: DH_PARAM_FILE)
@@ -36,12 +41,31 @@ class TestSSLDhParam < Test::Unit::TestCase
     assert_match(/^(DHE|EDH)/, Client.cipher_name)
   end
 
-  def test_dhparam_1_3
+  def test_dhparam_1_3_supplied
     omit_if(rbx?)
     omit("TLSv1_3 is unavailable") unless EM.const_defined? :EM_PROTO_TLSv1_3
 
     client = { client_unbind: true, ssl_version: %w(TLSv1_3) }
     server = { dhparam: DH_PARAM_FILE, cipher_list: "DHE,EDH", ssl_version: %w(TLSv1_3) }
+    client_server client: client, server: server
+
+    assert Client.handshake_completed?
+    assert Server.handshake_completed?
+
+    assert Client.cipher_name.length > 0
+    assert_equal Client.cipher_name, Server.cipher_name
+
+    # see https://wiki.openssl.org/index.php/TLS1.3#Ciphersuites
+    # may depend on OpenSSL build options
+    assert_equal "TLS_AES_256_GCM_SHA384", Client.cipher_name
+  end
+
+  def test_dhparam_1_3_auto
+    omit_if(rbx?)
+    omit("TLSv1_3 is unavailable") unless EM.const_defined? :EM_PROTO_TLSv1_3
+
+    client = { client_unbind: true, ssl_version: %w(TLSv1_3) }
+    server = { cipher_list: "DHE,EDH", ssl_version: %w(TLSv1_3) }
     client_server client: client, server: server
 
     assert Client.handshake_completed?
